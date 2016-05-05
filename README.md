6e5c9386522ed3acd959f81ba27521325ac61df2
Brian Snyder <bsnyder74@gmail.com>
2016-05-04 17:34:21 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # guidgen-js
-Creates a pseudo random 8 character guid used for server name creation.
+Creates a pseudo random 8 character guid used for server name creation in JavaScript.  
+
+Useful in automation / orchestration process.  
+
+As-is, will generate names like: `sbx-9a9ef82w`
---
+++ guidgen.js
@@ -0,0 +1,16 @@
+// Guidgen-js
+// Creates pseudo random 8 character guid prefixed with 'sbx'
+// Useful for automation / orchestration
+// Author: Brian Snyder
+
+function guid() {
+ function s4() {
+   return Math.floor((1 + Math.random()) * 0x10000)
+     .toString(16)
+     .substring(1);
+ }
+ return s4()+s4();
+}
+
+var miniguid = guid();
+var machineName = 'sbx' + '-' + miniguid
